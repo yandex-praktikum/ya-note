e3b21b5231c0488f48d90be2dd151f3ed490a01e
Iurii Cherkasov <cherkasoooov@gmail.com>
2025-12-02 21:01:27 +0100

    Add parameterized tests for route availability across user types

---
+++ notes/pytest_tests/test_routes.py
@@ -0,0 +1,69 @@
+# test_routes.py
+from http import HTTPStatus
+
+import pytest
+from django.urls import reverse
+
+
+@pytest.mark.parametrize(
+    'name',
+    [
+        'notes:home',
+        'users:login',
+        'users:signup',
+        pytest.param(
+            'users:logout',
+            marks=pytest.mark.skip(
+                reason="Anonymous logout not yet implemented."
+            ),
+            id='users:logout (skip)'
+        ),
+    ]
+)
+def test_pages_availability_for_anonymous_user(client, name):
+    url = reverse(name)
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:list', 'notes:add', 'notes:success')
+)
+def test_pages_availability_for_auth_user(not_author_client, name):
+    url = reverse(name)
+    response = not_author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+# Параметризуем тестирующую функцию:
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete'),
+)
+def test_pages_availability_for_author(author_client, name, note):
+    url = reverse(name, args=(note.slug,))
+    response = author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    # Предварительно оборачиваем имена фикстур
+    # в вызов функции pytest.lazy_fixture().
+    (
+            (pytest.lazy_fixture('not_author_client'), HTTPStatus.NOT_FOUND),
+            (pytest.lazy_fixture('author_client'), HTTPStatus.OK)
+    ),
+)
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete'),
+)
+def test_pages_availability_for_different_users(
+        parametrized_client, name, note, expected_status
+):
+    url = reverse(name, args=(note.slug,))
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status
+
